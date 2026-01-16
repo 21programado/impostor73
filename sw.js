432b833c0e5da86aeade68262bcf890780d7583e
21programado <21programado@gmail.com>
2026-01-15 22:37:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,17 @@
-const CACHE_NAME = "impostor-v2";
+const CACHE_NAME = "impostor-v3";
 
 const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
@@ -29,7 +28,7 @@ self.addEventListener("activate", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(
-      response => response || fetch(event.request)
+      cached => cached || fetch(event.request)
     )
   );
 });
